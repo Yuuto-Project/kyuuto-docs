17178daaf4da6c84d310cc7603e798e2c5388f51
Dé-Jà-Vu <iamdeja@outlook.com>
2020-07-16 15:43:08 +0300

    Revert "Implement landing page backbone (#1)"
    This reverts commit 728cf1844332f7912132f9fba67e994c1643e30e.

--- media/scripts/main.js
+++
@@ -1,6 +0,0 @@
-const [toggleGn] = document.getElementsByClassName("globalnav-toggle");
-const [globalNav] = document.getElementsByClassName("globalnav");
-
-toggleGn.onclick = () => {
-  globalNav.classList.toggle("is-active");
-};
